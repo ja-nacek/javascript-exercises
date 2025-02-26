42eedd66bf1a0fd762c7b70b7834344e9acd5deb
ja-nacek <145717125+ja-nacek@users.noreply.github.com>
2025-02-26 02:26:59 +0100

    Finished excercises 4, 5, and 6.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let i = 0;
+    let newArray = [];
+
+    for (i; i<array.length; i++) {
+        if (!args.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) { // -10, 4
+    let biggerNumber;
+    let smallerNumber;
+    let result = 0;
 
+    if (number1 < number2) { // -10 < 4 PRAVDA
+        smallerNumber = number1; // -10
+        biggerNumber = number2; // 4
+    } else {
+        biggerNumber = number1;
+        smallerNumber = number2;
+    }
+
+    if (number1 < 0 || number2 < 0 || !Number.isInteger(number1) || !Number.isInteger(number2)) {
+        return "ERROR";
+    }
+
+    for (smallerNumber; smallerNumber <= biggerNumber; smallerNumber++) { // -10; -10 <= 4; -10++
+        result = result + smallerNumber; // 0 + 
+    }
+    return result;
 };
 
 // Do not edit below this line
