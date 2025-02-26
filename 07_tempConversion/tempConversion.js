433e3cfbb24be4bb7ae74fe8cd4cae31078b2f33
ja-nacek <145717125+ja-nacek@users.noreply.github.com>
2025-02-26 03:03:15 +0100

    Finished excercises 7 and 8.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  // celsiusStepOne = farenheitTemp - 32;
+  // celsiusStepTwo = celsiusStepOne * 5;
+  // celsiusStepThree = celsiusStepTwo / 9;
+  // celsiusResultOne = Math.round(celsiusStepThree * 10);
+  // celsiusResultTwo = celsiusResultOne / 10;
+
+  celsiusResult = ((farenheitTemp - 32) * 5) / 9;
+  resultCelsius = Math.round(celsiusResult * 10) / 10;
+  return resultCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  farenheitResult = (celsiusTemp * 9) / 5 + 32;
+  resultFarenheit = Math.round(farenheitResult * 10) / 10;
+  return resultFarenheit;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (...args) {
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    result += args[i];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+  let result = 0;
+  let number1 = args[0];
+  let number2 = args[1];
+  return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (number1, number2) { // 2, 3
+  return Math.pow(number1, number2);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let product = 1;
+  for (let i = number; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
